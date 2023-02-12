43fdddb1fb1684de3585de7813f2c874ab475e7f
Juan P Mendieta C <jpmendietac@ut.edu.co>
2023-02-11 23:08:08 -0500

    second commit, Serializer in Django Rest Framework

--- inmuebles/urls.py
+++ inmuebles/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inmuebles/', include('inmuebleslist_app.urls'))
+    path('inmuebles/', include('inmuebleslist_app.api.urls')),
 ]
--- inmuebleslist_app/views.py
+++ inmuebleslist_app/views.py
@@ -1,27 +1,27 @@
-from django.shortcuts import render
-from inmuebleslist_app.models import Inmueble
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from inmuebleslist_app.models import Inmueble
+# from django.http import JsonResponse
 
-# Create your views here.
+# # Create your views here.
 
-def inmueble_list(request):
-    #This function returns all Inmueble objects as a JSON response.
-    inmuebles = Inmueble.objects.all() 
-    data = {
-        'inmuebles': list(inmuebles.values())
-    } # Se define como diccionario para usar la clase JsonResponse
+# def inmueble_list(request):
+#     #This function returns all Inmueble objects as a JSON response.
+#     inmuebles = Inmueble.objects.all() 
+#     data = {
+#         'inmuebles': list(inmuebles.values())
+#     } # Se define como diccionario para usar la clase JsonResponse
     
-    return JsonResponse(data)
+#     return JsonResponse(data)
 
-def inmueble_detalle(request, pk):
-    inmueble = Inmueble.objects.get(pk=pk)
-    data = {
-        'direccion': inmueble.direccion,
-        'pais': inmueble.pais,
-        'imagen': inmueble.imagen,
-        'activo': inmueble.activo,
-        'descripcion': inmueble.descripcion
-    }
+# def inmueble_detalle(request, pk):
+#     inmueble = Inmueble.objects.get(pk=pk)
+#     data = {
+#         'direccion': inmueble.direccion,
+#         'pais': inmueble.pais,
+#         'imagen': inmueble.imagen,
+#         'activo': inmueble.activo,
+#         'descripcion': inmueble.descripcion
+#     }
     
-    return JsonResponse(data)
+#     return JsonResponse(data)
     
