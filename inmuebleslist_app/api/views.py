e88968908ddbee35440ed3235a06b40bb81461d9
Juan P Mendieta C <jpmendietac@ut.edu.co>
2023-02-12 19:08:07 -0500

    Fourth commit, Http Status Codes in Django

--- inmuebleslist_app/api/views.py
+++ inmuebleslist_app/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from inmuebleslist_app.models import Inmueble
 from inmuebleslist_app.api.serializers import InmuebleSerializer
+
 
 @api_view(['GET', 'POST'])
 def inmueble_list(request):
@@ -10,6 +12,8 @@ def inmueble_list(request):
         inmuebles = Inmueble.objects.all()
         serializer = InmuebleSerializer(inmuebles, many=True)
         return Response(serializer.data)
+        
+        
     
     if request.method == 'POST':
         de_serializer = InmuebleSerializer(data=request.data)
@@ -25,9 +29,12 @@ def inmueble_list(request):
 def inmueble_detalle(request, pk):
     """Handles GET request for a single Inmueble instance, returning its serialized data."""
     if request.method == 'GET':
-        inmueble = Inmueble.objects.get(pk=pk)
-        serializer = InmuebleSerializer(inmueble)
-        return Response(serializer.data)
+        try:
+            inmueble = Inmueble.objects.get(pk=pk)
+            serializer = InmuebleSerializer(inmueble)
+            return Response(serializer.data)
+        except Inmueble.DoesNotExist:
+            return Response({'Error': 'El inmueble no existe'}, status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'PUT':
         inmueble = Inmueble.objects.get(pk=pk)
@@ -36,12 +43,14 @@ def inmueble_detalle(request, pk):
             de_serializer.save()
             return Response(de_serializer.data)
         else:
-            return Response(de_serializer.errors)
+            return Response(de_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
     if request.method == 'DELETE':
-        inmueble = Inmueble.objects.get(pk=pk)
-        inmueble.delete()
-        data = {
-            'resultado': True
-        }
-        return Response(data)
+        try:
+            inmueble = Inmueble.objects.get(pk=pk)
+            inmueble.delete()
+        
+        except Inmueble.DoesNotExist:
+            return Response({'Error': 'El inmueble no existe'}, status=status.HTTP_404_NOT_FOUND)
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
