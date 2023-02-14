ae96c5dedb1d4361d9b9fd9cb9b341414d83d25c
Juan P Mendieta C <jpmendietac@ut.edu.co>
2023-02-13 23:07:49 -0500

    Tenth commit, Relationship between Django models

--- inmuebleslist_app/models.py
+++ inmuebleslist_app/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Edificacion(models.Model):
-    direccion = models.CharField(max_length=250)
-    pais = models.CharField(max_length=150)
-    descripcion = models.CharField(max_length=500)
-    imagen = models.CharField(max_length=900)
-    activo = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.direccion
-    
 
 class Empresa(models.Model):
     nombre = models.CharField(max_length=250)
@@ -20,3 +9,18 @@ class Empresa(models.Model):
     
     def __str__(self):
         return self.nombre
+    
+class Edificacion(models.Model):
+    direccion = models.CharField(max_length=250)
+    pais = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=500)
+    imagen = models.CharField(max_length=900)
+    activo = models.BooleanField(default=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='edificacion')
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.direccion
+    
+
+
