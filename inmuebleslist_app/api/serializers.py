d1e279941af95c83887c955dea4a07e1e8d16b1d
Juan P Mendieta C <jpmendietac@ut.edu.co>
2023-02-13 20:51:25 -0500

    Sixth commit, Entity validation

--- inmuebleslist_app/api/serializers.py
+++ inmuebleslist_app/api/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from inmuebleslist_app.models import Inmueble
 
+def column_longitude(value):
+    if len(value) <2:
+        raise serializers.ValidationError('El valor es demasiado corta')
 
 class InmuebleSerializer(serializers.Serializer):
     """Serializer for the Inmueble model, converts data to Python structure for rendering into JSON or other content types."""
     id = serializers.IntegerField(read_only=True)
-    direccion = serializers.CharField()
-    pais = serializers.CharField()
+    direccion = serializers.CharField(validators=[column_longitude])
+    pais = serializers.CharField(validators=[column_longitude])
     descripcion = serializers.CharField()
     imagen = serializers.CharField()
     activo = serializers.BooleanField()
@@ -23,3 +26,17 @@ class InmuebleSerializer(serializers.Serializer):
         instance.activo = validated_data.get('activo', instance.activo)
         instance.save()
         return instance
+    
+    def validate(self, data):
+        if data['direccion']==data['pais']:
+            raise serializers.ValidationError('La dirección y el país no pueden ser el mismo')
+        
+        else: 
+            return data
+        
+    def validate_imagen(self, data):
+        if len(data) < 2:
+            raise serializers.ValidationError('La url es muy corta')
+        
+        else: 
+            return data
