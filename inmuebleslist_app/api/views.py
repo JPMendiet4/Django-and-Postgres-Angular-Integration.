bae9905a58e516b5fe2319e7196cabf84789968f
Juan P Mendieta C <jpmendietac@ut.edu.co>
2023-02-16 20:27:33 -0500

    Fourteenth commit, Generic methods in views api

--- inmuebleslist_app/api/views.py
+++ inmuebleslist_app/api/views.py
@@ -2,9 +2,28 @@ from rest_framework.response import Response
 #from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.views import APIView
-from inmuebleslist_app.models import Edificacion, Empresa
-from inmuebleslist_app.api.serializers import EdificacionSerializer, EmpresaSerializer
+from rest_framework import generics, mixins
+from inmuebleslist_app.models import Edificacion, Empresa, Comentario
+from inmuebleslist_app.api.serializers import EdificacionSerializer, EmpresaSerializer, ComentarioSerializer
 
+
+class ComentarioList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Comentario.objects.all()
+    serializer_class = ComentarioSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+
+class ComentarioDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset = Comentario.objects.all()
+    serializer_class = ComentarioSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 class EmpresaAV(APIView):
     
