d842f3ec81e31e723319164c4368b0b06e076266
Juan P Mendieta C <jpmendietac@ut.edu.co>
2023-02-18 09:27:50 -0500

    Nineteenth commit, Create user in Comments

--- inmuebleslist_app/api/views.py
+++ inmuebleslist_app/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics, mixins, viewsets
+from rest_framework.exceptions import ValidationError
 from inmuebleslist_app.models import Edificacion, Empresa, Comentario
 from inmuebleslist_app.api.serializers import EdificacionSerializer, EmpresaSerializer, ComentarioSerializer
 
@@ -11,11 +12,22 @@ from inmuebleslist_app.api.serializers import EdificacionSerializer, EmpresaSeri
 class ComentarioCreate(generics.CreateAPIView):
     serializer_class = ComentarioSerializer
     
+    def get_queryset(self):
+        return Comentario.objects.all()
+    
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         inmueble = Edificacion.objects.get(pk=pk)
         serializer.save(edificacion=inmueble)
             
+            
+        user = self.request.user
+        comentario_queryset = Comentario.objects.filter(edificacion=inmueble, comentario_user=user)
+        
+        if comentario_queryset.exists():
+            raise ValidationError('El usuario ya escribio un comentario para este inmueble')
+        
+        serializer.save(edificacion=inmueble, comentario_user=user)
 class ComentarioList(generics.ListCreateAPIView):
     #queryset = Comentario.objects.all()
     serializer_class = ComentarioSerializer
